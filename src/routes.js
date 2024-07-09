d97ba5985ee2cc3a50e0368484c999167fd2fba1
Nima Mousavian <mosaviannima@gmail.com>
2024-07-09 13:07:05 +0330

    user and product routes added.

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,16 @@ import Home from "./pages/home/Home";
 import UsersList from "./pages/usersList/UsersList";
 import NewUser from "./pages/newUser/NewUser";
 import Products from "./pages/products/Products";
+import User from "./pages/user/User";
+import Product from "./pages/product/Product";
 
 const routes = [
   { path: "/", element: <Home /> },
-  { path: "/users", element: <UsersList /> },
+  { path: "/users/", element: <UsersList /> },
+  { path: "/users/:id", element: <User /> },
   { path: "/newuser", element: <NewUser /> },
   { path: "/products", element: <Products /> },
+  { path: "/products/:id", element: <Product /> },
 ];
 
 export default routes;
